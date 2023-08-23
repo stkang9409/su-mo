b8be388bdf65fc80a02953dca2a5e7b1ed933de4
hoon25 <cchoon95@hanmail.net>
2023-08-23 22:06:51 +0900

    Formatting(CI): Apply prettier, eslint

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,4 +1,4 @@
-import SignOut from "@/components/sign-out";
+import SignOut from '@/components/sign-out';
 
 export default function Home() {
   return (
--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,12 @@
-import { getToken } from "next-auth/jwt";
-import { NextRequest, NextResponse } from "next/server";
+import { getToken } from 'next-auth/jwt';
+import { NextRequest, NextResponse } from 'next/server';
 
 export default async function middleware(req: NextRequest) {
   // Get the pathname of the request (e.g. /, /protected)
   const path = req.nextUrl.pathname;
 
   // If it's the root path, just render it
-  if (path === "/") {
+  if (path === '/') {
     return NextResponse.next();
   }
 
@@ -15,10 +15,10 @@ export default async function middleware(req: NextRequest) {
     secret: process.env.NEXTAUTH_SECRET,
   });
 
-  if (!session && path === "/protected") {
-    return NextResponse.redirect(new URL("/login", req.url));
-  } else if (session && (path === "/login" || path === "/register")) {
-    return NextResponse.redirect(new URL("/protected", req.url));
+  if (!session && path === '/protected') {
+    return NextResponse.redirect(new URL('/login', req.url));
+  } else if (session && (path === '/login' || path === '/register')) {
+    return NextResponse.redirect(new URL('/protected', req.url));
   }
   return NextResponse.next();
 }
